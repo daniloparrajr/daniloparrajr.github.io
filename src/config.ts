3709e6a257d1b9e5f20746335e221b7d81e2516d
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2023-08-03 12:56:00 +0800

    fix: og image path

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const SITE = {
   author: "Danilo Parra",
+  website: "https://daniloparrajr.com",
   description:
     "Hi, I'm Danilo! Welcome to my personal space in the internet, where I write my learning and thoughts about all things web development.",
   title: "Danilo Parra Jr - Web Developer",
