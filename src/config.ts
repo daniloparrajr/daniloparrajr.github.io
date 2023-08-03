347b0b9bb1c985bbab321a9a3f7333d9a083d9b9
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2023-08-03 09:53:55 +0800

    fix: update site description

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const SITE = {
   author: "Danilo Parra",
-  description: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  description:
+    "Hi, I'm Danilo! Welcome to my personal space in the internet, where I write my learning and thoughts about all things web development.",
   title: "Danilo Parra Jr - Web Developer",
   articlePerPage: 5,
 };
