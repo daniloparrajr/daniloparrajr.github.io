07c5bf1b43ce743be8327357d169de92b2c10364
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2023-08-07 15:32:34 +0800

    feat: add resume link and file

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -37,11 +37,11 @@ export default function ThemeToggle({ classes }) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-7"
-            viewBox="0 0 24 24"
+            viewBox="0 0 16 16"
           >
             <path
               fill="currentColor"
-              d="M6 2h8v2h-2v2h-2V4H6V2ZM4 6V4h2v2H4Zm0 10H2V6h2v10Zm2 2H4v-2h2v2Zm2 2H6v-2h2v2Zm10 0v2H8v-2h10Zm2-2v2h-2v-2h2Zm-2-4v-2h2v-2h2v8h-2v-4h-2Zm-6 0h6v2h-6v-2Zm-2-2h2v2h-2v-2Zm0 0V6H8v6h2Zm8-10h2v2h2v2h-2v2h-2V6h-2V4h2V2Z"
+              d="M7.85 3.015a5 5 0 1 1-4.585 7.712c1.403-.38 3.316-1.302 4.16-3.551c.552-1.474.584-2.938.425-4.16ZM13.456 8a6 6 0 0 0-6.21-5.996a.5.5 0 0 0-.475.592c.23 1.214.28 2.728-.283 4.228c-.8 2.134-2.802 2.84-4.077 3.071a.5.5 0 0 0-.361.71A6 6 0 0 0 13.456 8Z"
             />
           </svg>
         </>
@@ -51,11 +51,11 @@ export default function ThemeToggle({ classes }) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-7"
-            viewBox="0 0 24 24"
+            viewBox="0 0 16 16"
           >
             <path
               fill="currentColor"
-              d="M13 0h-2v4h2V0ZM0 11v2h4v-2H0Zm24 0v2h-4v-2h4ZM13 24h-2v-4h2v4ZM8 6h8v2H8V6ZM6 8h2v8H6V8Zm2 10v-2h8v2H8Zm10-2h-2V8h2v8Zm2-14h2v2h-2V2Zm0 2v2h-2V4h2Zm2 18h-2v-2h2v2Zm-2-2h-2v-2h2v2ZM4 2H2v2h2v2h2V4H4V2ZM2 22h2v-2h2v-2H4v2H2v2Z"
+              d="M8 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 8 1Zm0 10a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm0-1a2 2 0 1 1 0-4a2 2 0 0 1 0 4Zm6.5-1.5a.5.5 0 0 0 0-1h-1a.5.5 0 0 0 0 1h1ZM8 13a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 8 13ZM2.5 8.5a.5.5 0 0 0 0-1h-1a.5.5 0 0 0 0 1h1Zm.646-5.354a.5.5 0 0 1 .708 0l1 1a.5.5 0 1 1-.708.708l-1-1a.5.5 0 0 1 0-.708Zm.708 9.708a.5.5 0 1 1-.708-.707l1-1a.5.5 0 0 1 .708.707l-1 1Zm9-9.708a.5.5 0 0 0-.708 0l-1 1a.5.5 0 0 0 .708.708l1-1a.5.5 0 0 0 0-.708Zm-.708 9.708a.5.5 0 0 0 .708-.707l-1-1a.5.5 0 0 0-.708.707l1 1Z"
             />
           </svg>
         </>
--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,23 @@ export const SITE = {
   title: "Danilo Parra Jr - Web Developer",
   articlePerPage: 5,
 };
+
+export const MENU_LINKS = [
+  {
+    slug: "about",
+    name: "About",
+  },
+  {
+    slug: "articles",
+    name: "Articles",
+  },
+  {
+    slug: "files/resume-daniloparrajr-2023.pdf",
+    name: "Resume",
+    icon: {
+      name: "document-outline",
+      side: "left",
+    },
+    target: "_blank",
+  },
+];
