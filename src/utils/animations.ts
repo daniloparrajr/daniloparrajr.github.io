4c4303bb3c00e5ae30253dd2424b1cefa0f6a980
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2023-08-18 20:58:03 +0800

    refactor: animation to only animate elements with text content and images

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -13,23 +13,47 @@ export const hideContents = (container) => {
   return timeline;
 };
 
+/**
+ * Only allow elements with text content and images
+ *
+ * @param elements
+ * @returns
+ */
+const filterElements = (elements) => {
+  const elementsWithTextContent = [];
+  for (const element of elements) {
+    if (
+      (element.textContent &&
+        element.tagName !== "STYLE" &&
+        element.tagName !== "SCRIPT") ||
+      element.tagName === "IMG"
+    ) {
+      elementsWithTextContent.push(element);
+    }
+  }
+  return elementsWithTextContent;
+};
+
 export const fadeInContents = (container) => {
   const contents = container.querySelectorAll(".js-animate-content");
   const timeline = gsap.timeline();
+  const elements = [];
 
   timeline.delay(0.3);
 
   contents.forEach((content) => {
     if (content.children) {
-      timeline.to(content.children, {
-        opacity: 1,
-        y: 0,
-        duration: 0.8,
-        stagger: {
-          each: 0.2,
-        },
-      });
+      elements.push(filterElements(content.children));
     }
+  });
+
+  timeline.to(elements, {
+    opacity: 1,
+    y: 0,
+    duration: 0.8,
+    stagger: {
+      each: 0.2,
+    },
   });
 
   return timeline;
